d818e510479877f10fef135d5d26d28cfa2c1dc9
shaikalthaf011 <shaikalthaf7592@gmail.com>
2023-08-08 11:52:01 +0530

    petsearch

--- src/test/java/OR/HomepageOR.java
+++ src/test/java/OR/HomepageOR.java
@@ -24,17 +24,25 @@ public class HomepageOR extends TechnicalComponents {
 	 @FindBy(xpath="//div[@class='h-12 bg-white w-full max-w-2xl rounded-md relative']/input[@name='search']")
 	 public WebElement search;
 	 
-	 @FindBy(xpath="(//div[@class='flex flex-1 justify-end h-full space-x-5 md:space-x-10']/div[@class='h-full hidden md:flex items-center'])[2]/a[@href='/auth/v2']")
-	 public WebElement Login;
+	 @FindBy(xpath="//*[@id=\'__next\']/header/div[1]/div/div[2]/div[1]/div[2]/ul/li[1]")
+	public WebElement Search_text; 
 	 
-	  public void Highlightcard(WebElement element) {
+	@FindBy(xpath="//div[@class='w-full hidden lg:block relative']/nav/ul/li/span[contains(text(),'Shop by Pet')]")
+	public WebElement MOUSERHOVER_ACTIONS;
+	  
+	 public void Highlightcard(WebElement element) {
 		  highlightElement( element);
 	  }
       public void SearchBox(WebElement element,String text) {
 	 TechnicalComponents.type(element, text);
 }
-      public void Loginpage(WebElement element) {
-    	  TechnicalComponents.Click(element);
+     public void Text_search(WebElement element) {
+    	 TechnicalComponents.Click(element);
+     }
+     public void Actions_class(WebElement element) {
+    	 TechnicalComponents.Click(element);
+     }
+    	
+    }
 
-      }
-}
+
